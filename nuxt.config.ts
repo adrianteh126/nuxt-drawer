bcd6513809b8ef319c9553c014a63c990b6171c4
Kuan Kiat <adrianteh02@hotmail.com>
2023-12-13 15:20:36 +0800

    Enhance: Add icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: 'Nuxt Drawer',
+      meta: [
+        {
+          name: 'description',
+          content: 'A Nuxt 3 drawer.'
+        }
+      ],
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
+    }
+  },
   devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss']
 })
